b4ff27bae6f3b95c0e44f548d108935c69761f3f
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-08-01 18:06:31 -0300

    feat: conceitos de spread

---
+++ 3-spread.js
@@ -0,0 +1,28 @@
+const array = [5, 25, 'texto1', 'texto2', [5, 97]];
+
+// Spread Operator: ...
+/*Assim nós temos todas as informações da variavel array se mantendo, 
+ou seja que faz com que o novo endereço de memoria, tenha as mesmas 
+informações do endereço anterior, sendo assim a variavel array se manterá 
+da forma original, sem sobrepor o valor 5 com o valor 13513, ou sejá, só 
+a variavel array2 conterá o valor 13513 no indice 0 */
+
+const array2 = [ ...array ]; 
+array2[0] = 13513;
+
+console.log(array2);
+console.log(array);
+
+const object = {
+    chave1: 'valor1',
+    chave2: 5,
+    chave3: 'valor3',
+    chave4: [5, 97],
+    chave5: {
+        novaChave1: 5,
+    },
+};
+
+const object2 = { ...object };
+
+console.log(object2);
